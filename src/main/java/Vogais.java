18b2b7a1e88ed49db9636522faa5507df84c0961
Gustavo Leitão <gustavo.leitao@imd.ufrn.br>
2021-11-08 18:35:38 -0300

    Update Vogais.java

--- src/main/java/Vogais.java
+++ src/main/java/Vogais.java
@@ -7,21 +7,8 @@ public class Vogais {
      * @return quantidade de caracteres vogais incluídos no texto
      */
     public int contarVogais(String text){
-        var minusculo = text.toLowerCase();
-        var lookingFor = "aáàâãeéêèiíìîoóòôõuúùû";
-
-        var chars = minusculo.toCharArray();
-
-        int total = 0;
-
-        for (int i = 0; i < chars.length; i++) {
-            String item = String.valueOf(chars[i]);
-            if (lookingFor.indexOf(item) > -1){
-                total++;
-            }
-        }
-
-        return total;
+        // TODO
+        return 0;
     }
 
 }
